363b93d5b0f7388a6dbc277b31fdeca62d70df29
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-15 18:41:19 -0400

    Add Account model.

---
+++ MongoDbWebAPI/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbWebAPI.Models
+{
+    public class Account
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("account_id")]
+        public int AccountId { get; set; }
+
+        [BsonElement("limit")]
+        public int Limit { get; set; }
+
+        [BsonElement("products")]
+        public IEnumerable<int> Products { get; set; } = null!;
+    }
+}
