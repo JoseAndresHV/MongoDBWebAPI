7868a371933e44fb8f1675899553837632f4af30
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-15 18:50:21 -0400

    Add Transaction model.

---
+++ MongoDbWebAPI/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbWebAPI.Models
+{
+    public class Transaction
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("account_id")]
+        public int AccountId { get; set; }
+
+        [BsonElement("transaction_count")]
+        public int TransactionCount { get; set; }
+
+        [BsonElement("bucket_start_date")]
+        public DateTime BucketStartDate { get; set; }
+
+        [BsonElement("bucket_end_date")]
+        public DateTime BucketEndDate { get; set; }
+
+        [BsonElement("transactions")]
+        public IEnumerable<TransactionDetail> Transactions { get; set; } = null!;
+    }
+}
