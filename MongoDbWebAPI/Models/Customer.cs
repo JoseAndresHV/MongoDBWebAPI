c11fc16218019492472d55f30ec2429af8302c5b
JoseAndresHV <joseandreshurtadov@gmail.com>
2022-12-15 18:21:37 -0400

    Add Customer model.

---
+++ MongoDbWebAPI/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbWebAPI.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("username")]
+        public string Username { get; set; } = null!;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("address")]
+        public string Address { get; set; } = null!;
+
+        [BsonElement("birthdate")]
+        public DateTime Birthdate { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; } = null!;
+
+        [BsonElement("accounts")]
+        public IEnumerable<int> Accounts { get; set; } = null!;
+
+    }
+}
+
